50aa796347446dac04a2a5599a17d4cb75760fcc
Lesley Hogg <lesleyhogg@gmail.com>
2018-05-17 02:46:37 -0400

    tidying up

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,28 +4,24 @@ require 'net/http'
 
 class MoviesController < ApplicationController
   def index
-      today = Date.today.to_s
-      past = (Date.today - 14).to_s
+      #call API key in the .env file
       api = ENV['TMDB_API_KEY']
-      url = 'https://api.themoviedb.org/3/discover/movie?api_key=' + api + '&language=en-US&page=1&region=GR'
+      #parse the url into JSON
+      url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=' + api + '&region=GR'
       uri = URI(url)
       response = Net::HTTP.get(uri)
-
       @movies = JSON.parse(response)
 
+      #if there are results, pull info from the database hash
       unless @movies['results'].nil?
         @movies['results'].each do |movie|
-          #poster_url = 'https://image.tmdb.org/t/p/w500' + movie['poster_path'].to_s
-          #poster_uri = URI(url)
-          #poster_response = Net::HTTP.get(poster_uri)
-          #poster_hash = JSON.parse(poster_response)
-
           if Movie.where(tmdbid: movie['id'].to_i).empty?
             Movie.create(:title => movie['title'], :description => movie['overview'], :original_title => movie['original_title'], :tmdbid => movie['id'], :release_date => movie['release_date'].to_date, :poster_path => movie['poster_path'])
           end
         end
       end
 
+      #establish variable
       @dbmovies = Movie.all
     end
 end
